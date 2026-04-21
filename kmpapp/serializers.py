49066b5b6c88086639e746fc6e23dd470af43530
Devesh Taneja <devesh.taneja@gmail.com>
2026-04-21 17:38:20 +0530

    Add proper validation to restrict tag assignment to current user

--- kmpapp/serializers.py
+++ kmpapp/serializers.py
@@ -1,7 +1,46 @@
 from rest_framework import serializers
-from .models import Bookmark
+from .models import Bookmark, Tag
+
 
 class BookmarkSerializer(serializers.ModelSerializer):
+    tags = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=Tag.objects.all(),
+        required=False,
+        style={"base_template": "select_multiple.html"},
+    )
+
     class Meta:
         model = Bookmark
         fields = ['id', 'title', 'url', 'tags', 'user', 'created_at']
+        read_only_fields = ['user', 'created_at']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        request = self.context.get("request")
+
+        if request is not None and request.user.is_authenticated:
+            self.fields["tags"].queryset = Tag.objects.filter(
+                user=request.user
+            ).order_by("name")
+
+        print("REQUEST USER:", self.context.get("request"))
+        print("AUTH USER:", self.context["request"].user)
+
+        qs = Tag.objects.filter(user=request.user)
+        print("TAG COUNT:", qs.count())
+
+    def validate_tags(self, value):
+        request = self.context.get("request")
+        if request is None or not request.user.is_authenticated:
+            return value
+
+        user = request.user
+
+        for tag in value:
+            if tag.user != user:
+                raise serializers.ValidationError(
+                    "You can only assign your own tags."
+                )
+            return value
--- kmpapp/views.py
+++ kmpapp/views.py
@@ -2,10 +2,23 @@ from django.http import HttpResponse
 from .models import Bookmark
 from rest_framework import generics
 from .serializers import BookmarkSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class BookmarkListCreateView(generics.ListCreateAPIView):
-    queryset = Bookmark.objects.all()
     serializer_class = BookmarkSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Bookmark.objects.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+    
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["request"] = self.request
+        return context
 
 def home(request):
     return HttpResponse("Hello, Django!")
