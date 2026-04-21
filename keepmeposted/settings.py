9fed7dbbb929079b3069ba07361373c44700c1e7
Devesh Taneja <devesh.taneja@gmail.com>
2026-04-21 12:29:47 +0530

    Install Django REST Framework and add to installed apps

--- keepmeposted/settings.py
+++ keepmeposted/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kmpapp'
+    'kmpapp',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
