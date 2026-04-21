a2c37701a7f30bc0a488934db8a87d88286706e9
Devesh Taneja <devesh.taneja@gmail.com>
2026-04-21 12:55:44 +0530

    Add Bookmark list and create API endpoint using DRF

--- kmpapp/urls.py
+++ kmpapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import home, BookmarkListCreateView
 
 urlpatterns = [
     path('', views.home),
+    path("bookmarks/", BookmarkListCreateView.as_view(), name="bookmark_list"),
 ]
--- kmpapp/views.py
+++ kmpapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Bookmark, Tag
+from rest_framework import generics
+from .serializers import BookmarkSerializer
+
+class BookmarkListCreateView(generics.ListAPIView):
+    queryset = Bookmark.objects.all()
+    serializer_class = BookmarkSerializer
 
 def home(request):
     return HttpResponse("Hello, Django!")
