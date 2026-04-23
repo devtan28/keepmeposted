855fea15ace10e459b011384e370e95d6aeffc91
Devesh Taneja <devesh.taneja@gmail.com>
2026-04-23 10:27:45 +0530

    Install django-filter for API filtering

--- keepmeposted/settings.py
+++ keepmeposted/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'kmpapp',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
