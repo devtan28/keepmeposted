aa7771dd5141478c532a97f77c9e2ace768624a6
Devesh Taneja <devesh.taneja@gmail.com>
2026-04-23 13:04:43 +0530

    Enable CORS to allow frontend-backend communication

--- keepmeposted/settings.py
+++ keepmeposted/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
